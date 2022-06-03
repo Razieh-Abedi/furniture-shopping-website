5c2387e7168d65c5293afa2b893fee6576d1eea1
razinewworld <raziyeabedi@gmail.com>
2022-06-03 13:44:19 +0800

    tried the header main

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 
 import Login from "./pages/Login";
 
-
-
-import {BrowserRouter,Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import HeaderMain from "./components/HeaderMain";
 
 function App() {
-
-
   return (
     <BrowserRouter>
-       <Header />
+      <Header />
+      <HeaderMain />
       <Routes>
-
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/contact" element={<Contact/>} /> */}
-      
-
       </Routes>
-    
     </BrowserRouter>
   );
 }
 
 export default App;
 
-
-{/* <div className="App">
+{
+  /* <div className="App">
      
 
          <Login />
       </div>
 
-    </div> */}
+    </div> */
+}
