9d4fa36101a6685ff8ec05c95fd7a93701ccf892
razinewworld <raziyeabedi@gmail.com>
2022-08-22 11:23:07 +0800

    Updates

---
+++ precache-manifest.c9b34f4bd802f36fc1435c6bad294a5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e938ad8007dbb87edada",
+    "url": "/furniture-shopping-website/static/css/main.1978868e.chunk.css"
+  },
+  {
+    "revision": "e938ad8007dbb87edada",
+    "url": "/furniture-shopping-website/static/js/main.e938ad80.chunk.js"
+  },
+  {
+    "revision": "4acce407c256e430c35f",
+    "url": "/furniture-shopping-website/static/css/1.b28cf07d.chunk.css"
+  },
+  {
+    "revision": "4acce407c256e430c35f",
+    "url": "/furniture-shopping-website/static/js/1.4acce407.chunk.js"
+  },
+  {
+    "revision": "53ef7954c4381278f05e",
+    "url": "/furniture-shopping-website/static/js/runtime~main.53ef7954.js"
+  },
+  {
+    "revision": "fc36ed7db9d88e5901ca9992aaf83c92",
+    "url": "/furniture-shopping-website/static/media/letter.fc36ed7d.jpg"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/furniture-shopping-website/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/furniture-shopping-website/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "26f9d66bc59b49d1dc181af1085670c9",
+    "url": "/furniture-shopping-website/index.html"
+  }
+];
