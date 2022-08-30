510c72285105739669e59c195b474674db4c4635
razinewworld <raziyeabedi@gmail.com>
2022-08-30 21:09:17 +0800

    wrote the form handler for login form

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Login() {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const formHandler = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+    setUserInfo({ email: "", password: "",});
+  };
   return (
     <div className="container py-5 h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col-12 col-md-8 col-lg-6 col-xl-5">
           <div className="card shadow-2-strong" id="loginCard">
-            <div className="card-body p-5 text-center">
+            <form className="card-body p-5 text-center" onSubmit={formHandler}>
               <h3 className="mb-5 fs-1">Sign in</h3>
               <div class="form-floating mb-3">
                 <input
@@ -15,6 +24,11 @@ function Login() {
                   class="form-control"
                   id="floatingInput"
                   placeholder="name@example.com"
+                  name="email"
+                  value={userInfo.email}
+                  onChange={(e) =>
+                    setUserInfo({ ...userInfo, email: e.target.value })
+                  }
                 />
                 <label for="floatingInput">Email address</label>
               </div>
@@ -24,6 +38,11 @@ function Login() {
                   class="form-control"
                   id="floatingPassword"
                   placeholder="Password"
+                  name="password"
+                  value={userInfo.password}
+                  onChange={(e) =>
+                    setUserInfo({ ...userInfo, password: e.target.value })
+                  }
                 />
                 <label for="floatingPassword">Password</label>
               </div>
@@ -39,10 +58,7 @@ function Login() {
                   Remember password{" "}
                 </label>
               </div>
-              <button
-                className="btn btn-primary btn-lg "
-                type="submit"
-              >
+              <button className="btn btn-primary btn-lg " type="submit">
                 Login
               </button>
               <hr className="my-4" />
@@ -61,7 +77,7 @@ function Login() {
                 <i className="bi bi-facebook me-2"></i>
                 Sign in with facebook
               </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
